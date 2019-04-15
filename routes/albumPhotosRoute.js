c487f7eb4fffcfb13a7718404176c0f4548d1fc2
S-kaur <surinder1033@gmail.com>
2019-04-15 14:28:44 -0700

    database for createalbum modal created

--- routes/albumPhotosRoute.js
+++ routes/albumPhotosRoute.js
@@ -1,12 +1,58 @@
-'use strict'
+'use strict';
 
+
+var DB;
 var albumPhots = function(request,response){
     response.render("albumphotopage.hbs");
 }
 
 var createAlbum = function(request,response){
+
+
     console.log("/createAlbum route executed..")
-    response.render("create_album.hbs")
+
+    DB= request.app.locals.DB;
+    var albumName = request.body.albumName;
+    var isShared = request.body.exampleRadios;
+    var unixtime = 1301090400;
+    var newDate = new Date();
+    newDate.setTime(unixtime*1000);
+    var dateString = newDate.toUTCString();
+    var sharedPublicly;
+    if(isShared == "isPrivate"){
+        sharedPublicly = false;
+    } else {
+        sharedPublicly = true;
+    }
+    console.log(albumName +  " " + sharedPublicly +" " +dateString);
+
+    var emailAddress = "surinder12@gmail.com";
+    var albumLink = "www.google.com";
+    var album = {
+        emailAddress: emailAddress,
+        albumName: albumName,
+        dateCreated: dateString,
+        sharedPublicly: sharedPublicly,
+        albumLink: albumLink,
+        images:[]
+    };
+
+    DB.collection("albums").insertOne(album, function(error, result){
+        var alert = {
+            message: false
+        }
+            if(error) {
+            console.log("error occured while inserting data into the instructors collection");
+            response.render("create_album.hbs", alert);
+        } else {
+            alert.message = true;
+            response.render("create_album.hbs", alert);
+        }
+
+        
+
+    });
+    
 }
 
 exports.albumPhots = albumPhots;
