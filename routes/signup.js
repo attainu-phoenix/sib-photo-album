832ecc68f34facafda3b16e37586225830c54caf
[mayurcode] <[mayur.bhandare1994@gmail.com]>
2019-04-17 16:34:24 +0530

    Landing page modified

--- routes/signup.js
+++ routes/signup.js
@@ -1,7 +1,15 @@
 'use strict'
-
+var mongodb = require('mongodb');
+var DB;
 var signup = function(request,response){
-    response.render("index.hbs")
+   var userDetails = {
+   	name: request.body.name,
+   	email: request.body.email,
+   	password: request.body.password
+   };
+
+   console.log(userDetails);
+   response.render("index.hbs");
 
 }
 
